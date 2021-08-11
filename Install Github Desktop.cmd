878ea6c55a552d6b8f754222d473114a444ed4b7
heetbeet <sfstreicher@gmail.com>
2021-08-11 16:23:08 +0200

    Create Install Github Desktop.cmd

---
+++ Install Github Desktop.cmd
@@ -0,0 +1,13 @@
+@{' 2>nul&::'=''}>$nul <#
+@echo off &setlocal &set "cmdfile=%~dp0%~n0.cmd"
+@type "%cmdfile%" | powershell -NoP - &exit /b %errorlevel%
+#>
+
+Invoke-WebRequest -Uri 'https://central.github.com/deployments/desktop/desktop/latest/win32' -OutFile "$env:Temp/github-desktop.exe"
+Invoke-WebRequest -Uri 'https://github.com/heetbeet/GitHub-Desktop-context-menu/raw/main/Add%20GitHub-Desktop%20context%20menu.cmd' -OutFile "$env:Temp/add-context-menu.cmd"
+
+& "$env:Temp/github-desktop.exe"   | Out-Null
+& "$env:Temp/add-context-menu.cmd" | Write-Output
+
+Remove-Item -Path "$env:Temp/github-desktop.exe" -Force
+Remove-Item -Path "$env:Temp/add-context-menu.cmd" -Force
